d133a56f71d2a99db047e994b9d83ee2d7f536c7
Thales R. Menezes <thalesmenezes286@gmail.com>
2024-11-10 16:56:02 -0300

    feat: Product Controller and Product Record

---
+++ src/main/java/com/dev/productsapi/controllers/ProductController.java
@@ -0,0 +1,17 @@
+package com.dev.productsapi.controllers;
+
+import com.dev.productsapi.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ProductController {
+
+    @Autowired
+    ProductRepository productRepository;
+
+
+}
---
+++ src/main/java/com/dev/productsapi/dto/ProductRecordDTO.java
@@ -0,0 +1,6 @@
+package com.dev.productsapi.dto;
+
+import java.math.BigDecimal;
+
+public record ProductRecordDTO(String name, BigDecimal value) {
+}
